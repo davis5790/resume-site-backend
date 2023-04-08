093bdd80bcfc65d79ab61b6f4074ddd446b334c6
davis5790 <davis.ne5790@gmail.com>
2023-04-08 02:01:35 -0500

    first test with pytest tests.py

---
+++ tests.py
@@ -0,0 +1,21 @@
+import boto3, json
+
+client = boto3.client('dynamodb')
+TableName = 'Resume-Website-Stats'
+
+# Test to check that get_item returns a number
+
+# Test to check that list_tables returns a non empty list
+
+def get_views():
+    response = client.get_item(
+        TableName = TableName,
+        Key = {
+            'stat': {'S': "view-count"}
+        },
+        ProjectionExpression = 'quantity'
+    )
+    return int(response['Item']['quantity']['N'])
+
+def test_answer():
+    assert get_views() >= 0
