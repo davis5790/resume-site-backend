86ede15924fbbc7902a6cff3d978ba24c4988a16
davis5790 <davis.ne5790@gmail.com>
2023-04-08 00:12:08 -0500

    initial commit

---
+++ lambda.tf
@@ -0,0 +1,152 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda-role-policy"
+  role = aws_iam_role.iam_for_lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "view_counter.py"
+  output_path = "view_counter_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "view_counter_function_payload.zip"
+  function_name = "view_counter"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "view_counter.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.9"
+
+}
+
+/*
+ * An alias allows us to point our
+ * API gateway to a stable version of our function
+ * which we can update as we want
+ */
+resource "aws_lambda_alias" "api_lambda_alias" {
+  name             = "production"
+  function_name    = aws_lambda_function.test_lambda.arn
+  function_version = "$LATEST"
+
+  lifecycle {
+    ignore_changes = [
+      function_version
+    ]
+  }
+}
+
+/*
+ * An API gateway to expose our function
+ * to the Internet
+ */
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name          = "example-api-gateway"
+  protocol_type = "HTTP"
+  tags          = {}
+}
+
+resource "aws_cloudwatch_log_group" "api_gateway_log_group" {
+  name              = "/aws/api_gateway_log_group/${aws_apigatewayv2_api.api_gateway.name}"
+  retention_in_days = 14
+  tags              = {}
+}
+
+/*
+ * Default stage for our API gateway
+ * with basic access logs
+ */
+resource "aws_apigatewayv2_stage" "api_gateway_default_stage" {
+  api_id      = aws_apigatewayv2_api.api_gateway.id
+  name        = "$default"
+  auto_deploy = true
+  tags        = {}
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      status                  = "$context.status"
+      responseLatency         = "$context.responseLatency"
+      path                    = "$context.path"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
+}
+
+/*
+ * Integrate our function with our API gateway
+ * so that they can communicate
+ */
+resource "aws_apigatewayv2_integration" "api_gateway_integration" {
+  api_id             = aws_apigatewayv2_api.api_gateway.id
+  integration_uri    = "${aws_lambda_function.test_lambda.arn}:${aws_lambda_alias.api_lambda_alias.name}"
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  request_parameters = {}
+  request_templates  = {}
+}
+
+/*
+ * Tell our API gateway to forward all incoming
+ * requests (every path + HTTP verb) to our function
+ */
+resource "aws_apigatewayv2_route" "api_gateway_any_route" {
+  api_id               = aws_apigatewayv2_api.api_gateway.id
+  route_key            = "ANY /{proxy+}"
+  target               = "integrations/${aws_apigatewayv2_integration.api_gateway_integration.id}"
+  authorization_scopes = []
+  request_models       = {}
+}
+
+/*
+ * Allow our API gateway to invoke our function
+ */
+resource "aws_lambda_permission" "api_gateway_lambda_permission" {
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  qualifier     = aws_lambda_alias.api_lambda_alias.name
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/*"
+}
