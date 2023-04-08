6fe5605460eb83518ec7ac5238fb2b246d388df0
davis5790 <davis.ne5790@gmail.com>
2023-04-08 00:15:38 -0500

    second initial commit

---
+++ providers.tf
@@ -0,0 +1,4 @@
+// Specify settings for a given required provider
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ view_counter.py
@@ -0,0 +1,23 @@
+import json, boto3
+
+client = boto3.client('dynamodb')
+TableName = 'Resume-Website-Stats'
+
+def lambda_handler(event, context):
+
+    response = client.update_item(
+        TableName=TableName,
+        Key = {'stat': {'S': 'view-count'}},
+        UpdateExpression= 'ADD quantity :inc',
+        ExpressionAttributeValues = {
+            ':inc': {'N': '1'}
+        },
+        ReturnValues="UPDATED_NEW"
+        )
+        
+    value = response['Attributes']['quantity']['N']
+    
+    return {
+        'statusCode' : 200,
+        'body' : value
+    }
